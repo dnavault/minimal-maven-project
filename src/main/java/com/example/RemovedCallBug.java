05fc06eacbe6c5835a0fb96771150211593912d5
henry.coles <henry.coles@solarwinds.com>
2019-04-02 13:58:56 +0100

    initial commit

---
+++ src/main/java/com/example/RemovedCallBug.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RemovedCallBug {
+
+    boolean doSomething() {
+        return true;
+    }
+}
